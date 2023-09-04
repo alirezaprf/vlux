e7f1e0dd4b07be4846ff6ab2768b05448e14a0a4
alirezaprf <alirezaadvertise@gmail.com>
2023-09-04 19:10:12 +0330

    traffic

--- slave.py
+++ slave.py
@@ -1,8 +1,12 @@
+import os
 import json
 import logging
 import subprocess
 import time
 from threading import Thread
+from datetime import datetime as dt
+import pandas as pd
+import traffic
 
 import requests
 from websocket import WebSocketApp
@@ -25,12 +29,12 @@ def add_user_with_password(username, password):
         subprocess.run(['useradd', '-M', '-s', '/bin/false', username], check=True)
         log_text = f"User `{username}` created successfully."
         logger.info(log_text)
-        print(log_text)
+        # print(log_text)
         change_password_for_user(username, password)
     except subprocess.CalledProcessError as e:
         error_text = f"Error occurred on creating user `{username}`. {e}"
         logger.error(error_text)
-        print(error_text)
+        # print(error_text)
 
 
 def disable_ssh_for_user(username):
@@ -38,11 +42,11 @@ def disable_ssh_for_user(username):
         subprocess.run(['adduser', username, 'disabled_users'], check=True)
         log_text = f"User {username} added to disabled_users group."
         logger.info(log_text)
-        print(log_text)
+        # print(log_text)
         subprocess.run(['pkill', '-u', username], check=True)
         log_text = f"SSH access disabled for user `{username}`."
         logger.info(log_text)
-        print(log_text)
+        # print(log_text)
     except subprocess.CalledProcessError as e:
         error_text = f"Error occurred on disabling user `{username}`. {e}"
         logger.error(error_text)
@@ -54,7 +58,7 @@ def enable_ssh_for_user(username):
         subprocess.run(['gpasswd', '-d', username, 'disabled_users'], check=True)
         log_text = f"User `{username}` removed from disabled_users group."
         logger.info(log_text)
-        print(log_text)
+        # print(log_text)
     except subprocess.CalledProcessError as e:
         error_text = f"Error occurred on enabling user `{username}`. {e}"
         logger.error(error_text)
@@ -66,11 +70,11 @@ def delete_user(username):
         subprocess.run(['userdel', username], check=True)
         log_text = f"User `{username}` deleted."
         logger.info(log_text)
-        print(log_text)
+        # print(log_text)
     except subprocess.CalledProcessError as e:
         error_text = f"Error occurred on deleting user `{username}`. {e}"
         logger.error(error_text)
-        print(error_text)
+        # print(error_text)
 
 
 def change_password_for_user(username, new_password):
@@ -78,11 +82,11 @@ def change_password_for_user(username, new_password):
         subprocess.run(f'echo {username}:{new_password} | chpasswd', shell=True, check=True)
         log_text = f"Changed user `{username}` password to `{new_password}`."
         logger.info(log_text)
-        print(log_text)
+        # print(log_text)
     except subprocess.CalledProcessError as e:
         error_text = f"Error occurred on changing password of user `{username}`. {e}"
         logger.error(error_text)
-        print(error_text)
+        # print(error_text)
 
 
 def new_command(cmd: str):
@@ -130,14 +134,17 @@ def on_open(ws: WebSocketApp):
 
 
 def start_websocket():
+    global ws
     websocket = WebSocketApp(f"ws://{MASTER_URL}:{PORT}/ws/",
                              on_open=on_open,
                              on_message=on_message,
                              on_error=on_error,
                              on_close=on_close,
                              header={"Cookie": f"session={WS_TOKEN}"}, )
+
     ws_thread = Thread(target=websocket.run_forever)
     ws_thread.start()
+    ws = websocket
     return websocket
 
 
@@ -146,21 +153,52 @@ def get_ws_token():
     return response["access_token"]
 
 
-while True:
-    try:
-        WS_TOKEN = get_ws_token()
-        if WS_TOKEN == "":
-            logger.error("Token not valid")
-            print("Token not valid")
-            raise Exception("Token not valid")
-        ws = start_websocket()
-        time.sleep(1)
-        ws.send(json.dumps({"type": "fetch-users"}))
-        logger.info("Slave started successfully.")
-        print("__SLAVE__ Started Successfully")
-        break
-    except:
-        time.sleep(60 * 5)
+def send_ws_traffic(download_byuser: dict, upload_byuser: dict):
+    all_keys = set(list(download_byuser.keys()) + list(upload_byuser.keys()))
+    data = []
+    for k in all_keys:
+        data.append(
+            {
+                "username": k,
+                "download": download_byuser.get(k, 0),
+                "upload": upload_byuser.get(k, 0),
+            }
+        )
+    for _ in range(5):
+        try:
+            ws.send(
+                json.dumps(
+                    {"type": "add-traffic", "data": data}
+                )
+            )
+        except:
+            time.sleep(1)
+            continue
+        
+
+if __name__ == "__main__":
+    while True:
+        try:
+            WS_TOKEN = get_ws_token()
+            if WS_TOKEN == "":
+                logger.error("Token not valid")
+                print("Token not valid")
+                raise Exception("Token not valid")
+            ws = start_websocket()
+            time.sleep(1)
+            ws.send(json.dumps({"type": "fetch-users"}))
+            logger.info("Slave started successfully.")
+            print("__SLAVE__ Started Successfully")
+
+            folder_name = "tdump/"
+            traffic.mkdir(folder_name)
+            Thread(target=traffic.tcpdump, args=(folder_name, )).start()
+            Thread(target=traffic.netstat, args=(folder_name, )).start()
+            time.sleep(3)
+            Thread(target=traffic.files_analyze, args=(folder_name, send_ws_traffic)).start()
+            break
+        except:
+            time.sleep(60 * 5)
 
 # master_stats_thread = Thread(target=send_stats_mto_master, args=(ws,))
 # master_stats_thread.start()
---
+++ traffic.py
@@ -0,0 +1,141 @@
+from threading import Thread
+import os
+import pandas as pd
+import subprocess
+import time
+
+
+def mkdir(folder_name: str):
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+
+
+def tshark(filename):
+    subprocess.run("tshark -r " + filename + " -Y \"tcp\" -T fields -e tcp.dstport -e frame.len | awk '{arr[$1]+=$2}END{for(i in arr)print i, arr[i]}' > download",
+                   shell=True ,stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, check=True)
+    subprocess.run("tshark -r " + filename + " -Y \"tcp\" -T fields -e tcp.srcport -e frame.len | awk '{arr[$1]+=$2}END{for(i in arr)print i, arr[i]}' > upload",
+                   shell=True ,stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, check=True)
+    download = pd.read_csv('download', delimiter=' ', header=None, names=['port', 'dbyte'])
+    upload = pd.read_csv('upload', delimiter=' ', header=None, names=['port', 'ubyte'])
+    return {
+        "download": download.set_index('port'),
+        "upload": upload.set_index('port'),
+    }
+
+
+def read_netstat_file(filename):
+    n = pd.read_csv(filename, delimiter=' ', header=None, names=['port', 'user'])
+    n['port'] = n.port.apply(lambda x: x.split(":")[-1])
+    n = n.drop(n[n.port == 'ssh'].index)
+    n.port = n.port.astype(int)
+    return n.set_index('port')
+
+
+def tcpdump(folder_name):
+    while True:
+        subprocess.run("tcpdump -w %s.d -G 2", check=False, shell=True ,stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, cwd=folder_name)
+        time.sleep(0.01)
+
+
+def netstat(folder_name):
+    while True:
+        current_time = int(time.time())
+        subprocess.run("netstat -te | awk '$6 == \"ESTABLISHED\" {print $4, $9 $7}' > " + f"{current_time}.e", shell=True ,stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT,
+                        check=False, cwd=folder_name)
+        time.sleep(0.01)
+
+
+def to_mgb(d:dict):
+    return {k:round(v/1024/1024 , 2) for k,v in d.items() }
+
+def files_analyze(folder_name ,ws_send) :
+    while True:
+        # time.sleep(1)
+
+        files = os.listdir(folder_name)
+        files.sort(key=lambda x: int(x.split('.')[0]))
+        download_by_user = {}
+        upload_by_user = {}
+        last_download = None
+        last_upload = None
+
+        # for i , f in enumerate(files):
+        #     if f.split('.')[-1] == 'e':
+        #         break
+        # if i == len(files) - 1 :
+        #     continue
+        # files = files[:i+1]
+        files = files[:100]
+
+        for f in files:
+            file_type = f.split('.')[-1]
+            if file_type == 'd':
+                # ----------------------- Extract Traffic from d files ----------------------- #
+                r = tshark(folder_name + f)
+                # ---------------------------------------------------------------------------- #
+                download = r['download']
+                upload = r['upload']
+                # ---------------------------------------------------------------------------- #
+                if last_download is None:
+                    last_download = download
+                else:
+                    last_download = pd.concat((last_download, download)).groupby('port').aggregate('sum')
+                # ---------------------------------------------------------------------------- #
+                # ---------------------------------------------------------------------------- #
+                if last_upload is None:
+                    last_upload = upload
+                else:
+                    last_upload = pd.concat((last_upload, upload)).groupby('port').aggregate('sum')
+                # ---------------------------------------------------------------------------- #
+                # ---------------------------------------------------------------------------- #
+
+            else:
+                port_user = read_netstat_file(folder_name + f)
+                # ---------------------------------------------------------------------------- #
+                if last_download is not None:
+                    port_user = port_user.join(last_download)
+                    last_download = None
+                # ---------------------------------------------------------------------------- #
+                if last_upload is not None:
+                    port_user = port_user.join(last_upload)
+                    last_upload = None
+                # ---------------------------------------------------------------------------- #
+                port_user.fillna(0, inplace=True)
+                # ---------------------------------------------------------------------------- #
+
+                # ---------------------------------------------------------------------------- #
+                # ---------------------------------------------------------------------------- #
+                
+                for V in port_user.itertuples():
+                    # ---------------------------------------------------------------------------- #
+                    try:
+                        download_by_user.setdefault(V.user, 0)
+                        download_by_user[V.user] += V.dbyte
+                    except:
+                        pass
+                    # ---------------------------------------------------------------------------- #
+                    try:
+                        upload_by_user.setdefault(V.user, 0)
+                        upload_by_user[V.user] += V.ubyte
+                    except:
+                        pass
+            # ---------------------------------------------------------------------------- #
+            # ---------------------------------------------------------------------------- #
+            # ---------------------------------------------------------------------------- #
+            os.remove(folder_name + f)
+
+        # print(
+        #     "# ---------------------------------------------------------------------------- #\n",
+        #     f"{to_mgb(download_by_user) = }",
+        #     f"{to_mgb(upload_by_user) = }",
+        #     "\n# ---------------------------------------------------------------------------- #\n"
+        # )
+        ws_send(download_by_user , upload_by_user )
+
+if __name__ == "__main__":
+    folder_name = "tdump/"
+    mkdir(folder_name)
+    Thread(target=tcpdump, args=(folder_name, )).start()
+    Thread(target=netstat, args=(folder_name, )).start()
+    time.sleep(3)
+    Thread(target=files_analyze, args=(folder_name, )).start()
